db274e1aa0de5c0622c125c711123574f7c4d534
Tumo505 <tumokgabeng@gmail.com>
2025-11-02 00:46:28 +0200

    convert h5ad files into seurat object

---
+++ convert_to_seurat.R
@@ -0,0 +1,25 @@
+library(Seurat)
+library(tidyverse)
+library(R.utils)
+
+setwd("C:/Users/tumok/Documents/Projects/AnalyseSpatialTranscriptomics")
+
+getwd()
+list.files("./processed")
+
+
+gzip ("./processed/matrix.mtx")
+gzip ("./processed/barcodes.tsv")
+gzip ("./processed/features.tsv")
+
+Raw_data <- Read10X(data.dir = "./processed", gene.column = 1)
+metadata <- read.csv("./processed/metadata.csv")
+
+rownames(metadata) <- metadata[["X"]]
+metadata[["X"]]<-NULL
+
+processedSeuratObject <- CreateSeuratObject(counts = Raw_data, meta.data = metadata)
+
+view(processedSeuratObject@meta.data)
+
+saveRDS(processedSeuratObject, file="./")
